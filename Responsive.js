39fd690acdad8f65dcad56438b9ebf3ab4b18a83
KOTESHWARA RAO PAKEERI <87428991+Koteshwr@users.noreply.github.com>
2023-01-31 12:12:52 +0530

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -2,13 +2,8 @@
 
 
 console.log(screen.width)
-if(screen.width<=400){
-    document.getElementsByClassName("container")[0].classList.remove("Player")
-    tab1.style.width = '100%'
-    tab1.style.height = '50%'
-    tab2.style.width = '0%'
-    tab2.style.height = '0%'
-
-    btn2.style.visibility = "hidden"
-    sItem.style.height = "75vh"  
+if (screen.width <= 400) {
+    document.getElementsByClassName("songListItems")[0].classList.add("songListItems_mobile")
+    document.getElementsByClassName("songListItems")[0].classList.remove("songListItems")
+    
 }
