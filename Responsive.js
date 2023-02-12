7f04b4cc9b07af2b55c3e8369c924b4c18105ab5
KOTESHWARA RAO PAKEERI <87428991+Koteshwr@users.noreply.github.com>
2023-02-12 23:43:48 +0530

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -3,23 +3,25 @@
 
 let page = document.getElementById("mobile-home");
 let musiclist = document.getElementById("list-icon");
-page.style.height = "75vh";
-
+page.style.visibility = "collapse";
+musiclist.style.visibility = "hidden";
 if (screen.width <= 400) {
+    page.style.visibility = "visible";
+    musiclist.style.visibility = "visible";
+    page.style.height = "75vh";
+    page.style.width = "100%";
     document.getElementsByClassName("songListItems")[0].classList.add("songListItems_mobile");
     document.getElementsByClassName("songListItems")[0].classList.remove("songListItems");
-  
-
 }
 musiclist.addEventListener('click', () => {
 
-    if ( page.style.height == '75vh') {
+    if (page.style.height == '75vh') {
         // page.style.visibility = "collapse";
         page.style.height = '0vh';
         musiclist.classList.remove("fa-bars");
         musiclist.classList.add("fa-square-xmark");
     }
-    else{
+    else {
         // page.style.visibility = "visible";
         page.style.height = '75vh';
         musiclist.classList.add("fa-bars");
